5980584df08abcc8f888d32d234dc3cf40faa47d
Naman Pandey <2492004namanpandey@gmail.com>
2024-06-14 06:57:27 +0530

    update signup route

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,7 +2,7 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import { sendemail } from "@/helpers/mailer";
+import { sendEmail } from "@/helpers/mailer";
 
 connect();
 
@@ -10,10 +10,12 @@ export async function POST(request: NextRequest) {
   try {
     const reqBody = await request.json();
     const { username, email, password } = reqBody;
-    // validation
+
     console.log(reqBody);
 
+    //check if user already exists
     const user = await User.findOne({ email });
+
     if (user) {
       return NextResponse.json(
         { error: "User already exists" },
@@ -21,6 +23,7 @@ export async function POST(request: NextRequest) {
       );
     }
 
+    //hash password
     const salt = await bcryptjs.genSalt(10);
     const hashedPassword = await bcryptjs.hash(password, salt);
 
@@ -33,20 +36,16 @@ export async function POST(request: NextRequest) {
     const savedUser = await newUser.save();
     console.log(savedUser);
 
-    // send verification email
-    await sendemail({ email, emailType: "VERIFY", userID: savedUser._id });
+    //send verification email
+
+    await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id });
 
     return NextResponse.json({
-      message: "User registered successfully",
+      message: "User created successfully",
       success: true,
       savedUser,
     });
   } catch (error: any) {
-    return NextResponse.json(
-      {
-        error: error.message,
-      },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
